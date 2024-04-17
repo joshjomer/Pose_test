1d75d113c9fb226d725e342e53b7a3d4ef06add6
Josh Joy <36846262+joshjomer@users.noreply.github.com>
2024-04-17 11:16:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ import {
   } from "https://cdn.skypack.dev/@mediapipe/tasks-vision@0.10.0";   
 
   let poseLandmarker = undefined;
+  let enableWebcamButton = undefined;
   let runningMode = "IMAGE";
   
   const createPoseLandmarker = async () => {
@@ -38,7 +39,7 @@ const hasGetUserMedia = () => !!navigator.mediaDevices?.getUserMedia;
 // If webcam supported, add event listener to button for when user
 // wants to activate it.
 if (hasGetUserMedia()) {
-    let enableWebcamButton = document.getElementById("webcamButton");
+    enableWebcamButton = document.getElementById("webcamButton");
     enableWebcamButton.addEventListener("click", enableCam);
   } else {
     console.warn("getUserMedia() is not supported by your browser");
